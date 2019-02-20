3e5fa44531330a53d2a641e7140900a92449125d
laxmithoodi <laxmir.117@gmail.com>
2019-02-19 19:59:42 -0500

    Done

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,46 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+
+    private  String name;
+    private  Double price;
+    private Long id;
+
+
     public Item() {
+        this.name = "";
+        this.price= 0.01;
+        this.id = 0L;
     }
 
     public Item(Long id, String name, Double price) {
+
+        this.name = name;
+        this.price= price;
+        this.id = id;
+
+
     }
 
-    public String getName() {
-        return null;
+    public String getName() { return name;
     }
 
     public void setName(String name) {
+        this.name = name;
+
     }
 
-    public Long getId() {
-        return null;
+    public Long getId() { return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
-    public Double getPrice() {
-        return null;
+    public Double getPrice() { return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,29 @@
 package rocks.zipcode.io;
 
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+
+        Item[] items;
+
     public ItemSorter(Item[] items) {
+
+        this.items = items;
+
+
+
+
+
+
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+
+        Arrays.sort(this.items, comparator);
+        return items;
     }
 }
